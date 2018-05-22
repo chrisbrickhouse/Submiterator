e2f0a98d41b66ee85b840c59564752cf6ad03c6e
Christian Brickhouse <ctb77@stanford.edu>
2018-05-21 17:48:53 -0700

    Starting batch implementation.

--- submiterator.py
+++ submiterator.py
@@ -2,6 +2,7 @@
 
 import csv, codecs, cStringIO
 import os, json, re, argparse
+import time
 
 import sys
 csv.field_size_limit(sys.maxsize)
@@ -88,7 +89,7 @@ def write_csv(data, filename, delimiter=','):
 def main():
     parser = argparse.ArgumentParser(description='Interface with MTurk.')
     parser.add_argument("subcommand", choices=['posthit', 'getresults', 'reformat',
-        "preparefiles", "anonymize"],
+        "preparefiles", "anonymize", "postbatches"],
         type=str, action="store",
         help="choose a specific subcommand.")
     parser.add_argument("nameofexperimentfiles", metavar="label", type=str, nargs="+",
@@ -111,6 +112,8 @@ def main():
             getresults(label)
             anonymize(label + ".results")
             make_invoice(label)
+        elif subcommand == "postbatches":
+            postbatches(label)
         elif subcommand == "reformat":
             try:
                 try:
@@ -225,15 +228,123 @@ def posthit(label):
         """
         )
 
-def getresults(label):
-    os.system("""
+def getresults(label, batch=False):
+    results = os.popen("""
         HERE=`pwd`
         cd $MTURK_CMD_HOME/bin
 
         NAME_OF_EXPERIMENT_FILES=""" + label + """
+        export NAME_OF_EXPERIMENT_FILES
         label="$HERE/$NAME_OF_EXPERIMENT_FILES"
         ./getResults.sh -successfile "$label.success" -outputfile "$label.results"
         """)
+    for line in results:
+        print(line[:-1])
+    if batch:
+        completion_data = results[6].split(' ')[2].split('/')
+        return(completion_data)
+
+def postbatches(label):
+    config_file = label + ".config"
+    settings = open(config_file, 'r')
+    json_string = settings.read()
+    settings.close()
+    config_dict = json.loads(re.sub("\n", "", json_string))
+    total = int(config_dict['totalassignments'])
+    per = int(config_dict['numberofassignments'])
+    remainder = total%per
+    if remainder != 0:
+        n_batches = (total/per)+1
+    else:
+        n_batches = total/per
+    i = 0
+    while i < n_batches:
+        if i + 1 == n_batches:
+            per = remainder
+        locationofCLT = os.environ['MTURK_CMD_HOME']
+        if not os.path.exists(locationofCLT) or locationofCLT == '/':
+            raise Exception("Error: please set your 'MTURK_CMD_HOME' environment variable to your AWS directory.")
+
+        if dict["rewriteProperties"] == "yes":
+            old_properties_file = open(locationofCLT + "/bin/mturk.properties", 'r').readlines()
+            backup = open(locationofCLT + "/bin/mturk.properties.backup", 'w')
+            for line in old_properties_file:
+                backup.write(line + '\n')
+            backup.close()
+            new_properties_file = open(locationofCLT + "/bin/mturk.properties", 'w')
+            if (dict["liveHIT"] == "yes"):
+                for line in old_properties_file:
+                    if "://mechanicalturk.sandbox.amazonaws.com/?Service=AWSMechanicalTurkRequester" in line:
+                        new_properties_file.write("# service_url=https://mechanicalturk.sandbox.amazonaws.com/?Service=AWSMechanicalTurkRequester\n")
+                    elif "://mechanicalturk.amazonaws.com/?Service=AWSMechanicalTurkRequester" in line:
+                         new_properties_file.write("service_url=https://mechanicalturk.amazonaws.com/?Service=AWSMechanicalTurkRequester\n")
+                    else:
+                        new_properties_file.write(line)
+            else:
+                for line in old_properties_file:
+                    if "://mechanicalturk.sandbox.amazonaws.com/?Service=AWSMechanicalTurkRequester" in line:
+                        new_properties_file.write("service_url=https://mechanicalturk.sandbox.amazonaws.com/?Service=AWSMechanicalTurkRequester\n")
+                    elif "://mechanicalturk.amazonaws.com/?Service=AWSMechanicalTurkRequester" in line:
+                        new_properties_file.write("# service_url=https://mechanicalturk.amazonaws.com/?Service=AWSMechanicalTurkRequester\n")
+                    else:
+                        new_properties_file.write(line)
+            new_properties_file.close()
+            print "Old mturk.properties file backed up at " + locationofCLT + "/bin/mturk.properties.backup"
+
+        # write the .question file, which tells MTurk where to find your external HIT.
+        question = open(output_dir + nameofexperimentfiles + ".question", 'w')
+        question.write("<?xml version='1.0'?><ExternalQuestion xmlns='http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd'><ExternalURL>" + dict["experimentURL"] + "</ExternalURL><FrameHeight>"+ dict["frameheight"] +"</FrameHeight></ExternalQuestion>")
+        question.close()
+
+        #write the .properties file.
+        properties = open(output_dir + nameofexperimentfiles + ".properties", 'w')
+        properties.write(
+            "title: " + config_dict["title"] + \
+            "\ndescription: " + config_dict["description"] + \
+            "\nkeywords: " + config_dict["keywords"] + \
+            "\nreward: " + config_dict["reward"] + \
+            "\nassignments: " + per + \
+            "\nannotation: ${condition}" + \
+            "\nassignmentduration:" + config_dict["assignmentduration"] + \
+            "\nhitlifetime:" + config_dict["hitlifetime"] + \
+            "\nautoapprovaldelay:" + config_dict["autoapprovaldelay"]
+            )
+        if (config_dict["USonly?"] == "y" or \
+                config_dict["USonly?"] == "Y" or \
+                config_dict["USonly?"] == "yes" or \
+                config_dict["USonly?"] == "Yes" or \
+                config_dict["USonly?"] == "true" or \
+                config_dict["USonly?"] == "True" or \
+                config_dict["USonly?"] == "T" or \
+                config_dict["USonly?"] == "1"):
+            properties.write(
+                "\nqualification.1:00000000000000000071"+\
+                "\nqualification.comparator.1:EqualTo"+\
+                "\nqualification.locale.1:US"+\
+                "\nqualification.private.1:false"
+                )
+        if (dict["minPercentPreviousHITsApproved"] != "none"):
+            properties.write("\nqualification.2:000000000000000000L0\nqualification.comparator.2:GreaterThanOrEqualTo\nqualification.value.2:" + dict["minPercentPreviousHITsApproved"] + "\nqualification.private.2:false")
+        if (dict["doesNotHaveQualification"] != "none"):
+            properties.write("\nqualification.3:" + dict["doesNotHaveQualification"] + "\nqualification.comparator.3:DoesNotExist\nqualification.private.3:true")
+        properties.close()
+
+        #write the .input file. "conditions::" in the file experiment-settings.txt can be followed by any number of condition names, separated by a comma.
+        input = open(output_dir + nameofexperimentfiles + ".input", 'w')
+        input.write("condition\n")
+        num = 1
+        conditions = dict["conditions"]
+        conditionlist = conditions.split(",")
+        for x in conditionlist:
+            input.write(submiterator_stringify(num) + " " + x + " \n")
+            num = num + 1
+        input.close()
+        posthit(label)
+        time.sleep(300) # Sleep for 5 minutes
+        while getResults(label,True)[0] < per:
+            time.sleep(300)
+        i+=1
+
 
 def anonymize(original_data_filename):
     workers = {}
